218e3556ce47149332f64b91a19d643767f5d426
heneeeee-crypto <heneeeee@gmail.com>
2026-04-14 15:13:25 +0300

    Delete save.js

--- save.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const payload = JSON.parse(process.env.GITHUB_EVENT_PAYLOAD || '{}');
-if(!payload.client_payload) process.exit(0);
-const { key, list } = payload.client_payload;
-const data = JSON.parse(fs.readFileSync('data/data.json','utf8'));
-data[key] = list;
-fs.writeFileSync('data/data.json', JSON.stringify(data, null, 2));
